5b1a10e0f6059aeb3c79b4b8965ca55c416fe306
milesL1 <142256028+milesL1@users.noreply.github.com>
2025-01-28 16:54:21 +0000

    1/28 end of class

--- pleasework2.py
+++ pleasework2.py
@@ -1,24 +1,28 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QPixmap
 from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self): #This is for some window stuff but also defining elements here
         super().__init__()#define any objects/elements that youre using here
         self.setWindowTitle("JaRVIS") 
-        self.setGeometry(500, 500, 1000, 1000) #first 2 are x/y offset. How far the window is from top left corner. last 2 are window size
-        #Background must be first inorder to be background
+        self.setGeometry(500, 500, 1000, 750) #(Xoffset, YOffset, width, height)
+        Background = QLabel(self) #Background must be first
         label = QLabel("Welcome to Jarvis", self)
         self.button = QPushButton("voice search", self)#making buttons "self.bla" means that they arent local to that function and can interact with other functions
         self.initUI()
 
     def initUI(self): #define elements and their characteristics here
+        background.setGeometry(0 ,0, 1000, 750) #background photo
+        pixmap = QPixmap("Jarvis_Background1.jpg")
+        background.setPixmap(Pixmap)
+        background.setScaledContents(True)
+
         welcome = Qlabel("Welcome to Jarvis", self)
         welcome.setfont(Qfont("arial", 40))
-        welcome.setgeometry(0, 0, 500, 100) #xy offset and dimensions of the label inside the window
+        welcome.setgeometry(250, 0, 500, 100) 
         label.setStyleSheet("color: white;"
-                            "background-color: #383071;" #A dark blue or purple
                             "font-weight: bold;"
                             "font-style: italic;")
         label.setAlignment(Qt.AlignCenter | Qt.AlignTop) #alligns text (label) to top and center of its space
